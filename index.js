02429f287338e074adb5c08bee87f4d2046a5aaf
Faheem <mohdfaheemahmad5@gmail.com>
2023-03-05 03:17:40 +0530

    better code organization

--- index.js
+++ index.js
@@ -1,88 +1,45 @@
-console.log(`hello`);
+let difficulty = document.getElementById("difficulty");
+let difficultyShow = document.querySelector("#difficultyContainer > p");
+let obstacle = document.getElementById("obstacle");
+let playBtn = document.querySelector("#btnContainer button");
 
 
 
-const canvas = document.getElementById("myCanvas");
-const HEIGHT = 600;
-const WIDTH = 600;
-canvas.width = WIDTH;
-canvas.height = HEIGHT;
+difficulty.addEventListener("input", (e) => {
+    setMssg();
 
-const ctx = canvas.getContext("2d");
-const FPS = 10;
-const snakeHeadSize = 20;
-
-
-
-let then = Date.now();
-
-let gamePause = false;
-
-snake = new Snake(WIDTH,HEIGHT,snakeHeadSize);
-
-function gameLoop() {
-
-    requestAnimationFrame(gameLoop)
-    const elapsed = Date.now() - then;
-
-    if (elapsed > 1000 / FPS) { 
-        then = Date.now() - (elapsed % (1000 / FPS));
-
-        if(!gamePause){
-            ctx.clearRect(0,0,WIDTH,HEIGHT)
-            snake.show();
-            snake.eat();
-            snake.death();
-            snake.update();
-        }
-
-
-
-    }
-
-
-
-}
-
-requestAnimationFrame(gameLoop);
-
-document.addEventListener("keydown",(e)=>{
-    // console.log(e);
-    if(e.key == "ArrowUp" || e.key.toLowerCase() == "w" || e.key == "8"){
-        if(!snake.checkInvalidMove(0,-1)){
-            snake.xSpeed = 0;
-            snake.ySpeed = -1;
-        }
-    }else if(e.key == "ArrowDown" || e.key.toLowerCase() == "s" || e.key == "5"){
-        if(!snake.checkInvalidMove(0,1)){
-            snake.xSpeed = 0;
-            snake.ySpeed = 1;
-        }
-    }else if(e.key == "ArrowLeft" || e.key.toLowerCase() == "a" || e.key == "4"){
-
-        if(!snake.checkInvalidMove(-1,0)){
-            snake.xSpeed = -1;
-            snake.ySpeed = 0;
-        }
-        
-    }else if(e.key == "ArrowRight" || e.key.toLowerCase() == "d" || e.key == "6"){
-        if(!snake.checkInvalidMove(1,0)){
-            snake.xSpeed = 1;
-            snake.ySpeed = 0;
-        }
-    }else if(e.key == " "){
-            if(gamePause){
-                gamePause = false;
-            }else{
-                gamePause = true;
-            }
-    }
-    
-    else{
-        snake.grow();
-        snake.grow();
-        snake.grow();
-    }
-    
 })
 
+
+
+difficulty.addEventListener("change", (e) => {
+    setMssg();
+
+})
+
+setMssg();
+
+function setMssg() {
+    if (difficulty.value == 0) {
+        difficultyShow.style.color = "green";
+        difficultyShow.innerHTML = "Easy";
+    } else if (difficulty.value == 1) {
+        difficultyShow.style.color = "yellow";
+        difficultyShow.innerHTML = "Medium";
+    } else if (difficulty.value == 2) {
+        difficultyShow.style.color = "orange";
+        difficultyShow.innerHTML = "Hard";
+    } else if (difficulty.value == 3) {
+        difficultyShow.style.color = "red";
+        difficultyShow.innerHTML = "Very Hard";
+    } else if (difficulty.value == 4) {
+        difficultyShow.style.color = "purple";
+        difficultyShow.innerHTML = "Impossible";
+    }
+}
+
+
+playBtn.addEventListener("click", (e) => {
+    window.location = `./snake.html?level=${difficulty.value}&obstacle=${obstacle.checked}`;
+
+})
